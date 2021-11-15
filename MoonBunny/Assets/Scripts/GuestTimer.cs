809b4113fb88068c646c8cffcf529fce770f1874
ksb3458 <ksb3458@naver.com>
2021-11-15 17:32:49 +0900

    life 생성
    -GuestTimer.cs 수정

--- MoonBunny/Assets/Scripts/GuestTimer.cs
+++ MoonBunny/Assets/Scripts/GuestTimer.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class GuestTimer : MonoBehaviour
 
 {
-    Animator anim; 
+    Animator anim;
+    public int num = 0;
+    public GameObject[] life = new GameObject[3];
+    public Sprite emptyLife;
+    public GameObject timeOver;
 
     private void ChangeColor()
     {
@@ -23,6 +28,14 @@ public class GuestTimer : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         anim.SetBool("GuestOn", true);
+
+        if (num == 3)
+        {
+            Debug.Log("Game Over");
+            timeOver.SetActive(true);
+            Invoke("gameOver", 2.0f);
+        }
+
         Debug.Log("timer on");
     }
 
@@ -30,6 +43,22 @@ public class GuestTimer : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         anim.SetBool("GuestOn", false);
+
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            life[num].GetComponent<SpriteRenderer>().sprite = emptyLife;
+            num++;
+        }
+
         Debug.Log("timer off");
+    }
+
+    void gameOver()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 }
