aab71acf69aba602e7af0620f10431cba332ee9f
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2022-05-31 05:47:10 +0900

    상점 스크롤뷰 완료

--- MoonBunny/Assets/Scripts/shop/ScrollView.cs
+++ MoonBunny/Assets/Scripts/shop/ScrollView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,44 +6,197 @@ using UnityEngine.UI;
 
 public class ScrollView : MonoBehaviour
 {
-    private ScrollRect _scrollRect;
-    [SerializeField] private ScrollButton leftButton;
-    [SerializeField] private ScrollButton rightButton;
-    [SerializeField] private float scrollSpeed = 0.01f;
+    public Sprite[] sprite = new Sprite[9];
+    public Sprite lockSprite;
+    public GameObject content;
+    private int num = 0, myCarrot;
+    private int[] xPos = { 40, -150, -345, -536, -730, -922, -1113, -1306, -1500 };
+    private int[] clickPos = { 62, -227, -515, -805, -1093, -1382, -1670, -1959, -2248 };
+    private int yPos = 10;
+
+    public Sprite[] price = new Sprite[9]; //배경별 가격
+    public Sprite[] stateBG = new Sprite[2]; //0:적용중 1:구매완료
+    public Sprite[] setBG = new Sprite[2]; //0:적용 1:구매
+    public GameObject oriState, oriSet, BackGround;
+    public GameObject purchasePanel, right, left;
+    public Text carrot;
+    private bool check = false;
+    public static bool touchChk = false;
+    private int[] priceBG = new int[9];
+    private int[] state = new int[9]; //0:적용중 1:구매완료 2:가격 3:자물쇠
 
     // Start is called before the first frame update
     void Start()
     {
-        _scrollRect = GetComponent<ScrollRect>();
+        for(int i=0; i<sprite.Length; i++)
+        {
+            if (sprite[i] == null)
+            {
+                sprite[i] = lockSprite;
+                state[i] = 3;
+            }
+
+            else state[i] = 2;
+            content.transform.GetChild(i).GetComponent<Image>().sprite = sprite[i];
+        }
+        state[0] = 0;
+        priceBG[0] = 50000; //수정 필요
+        priceBG[1] = 50000; //수정 필요
+        priceBG[4] = 60000; //수정 필요
+
+        check = true;
+        purchasePanel.SetActive(false);
+        Int32.TryParse(carrot.text, out myCarrot);
+    }
+
+    public void ScrollLeft()
+    {
+        if (num == 0)
+            num -= 0;
+        else
+        {
+            num -= 1;
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(xPos[num], yPos);
+            check = true;
+        }
+    }
+
+    public void ScrollRight()
+    {
+        if (num == sprite.Length - 1)
+            num += 0;
+        else
+        {
+            num += 1;
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(xPos[num], yPos);
+            check = true;
+        }
+    }
+
+    public void setBtn()
+    {
+        if (state[num] == 0) { }
+
+        else if (state[num] == 1) //적용
+        {
+            for (int i = 0; i < state.Length; i++)
+            {
+                if (state[i] == 0)
+                    state[i] = 1;
+            }
+            state[num] = 0;
+            check = true;
+        }
+
+        else if (state[num] == 2) //구매
+        {
+            openPanel();
+        }
+
+        else if (state[num] == 3) { }
+    }
+
+    public void purchaseOK()
+    {
+        if (myCarrot >= priceBG[num])
+        {
+            myCarrot -= priceBG[num];
+            carrot.text = myCarrot.ToString();
+            state[num] = 1;
+            check = true;
+            closePanel();
+        }
+        else
+        {
+            Debug.Log("소지 당근이 적습니다.");
+            closePanel();
+        }
+    }
+
+    public void purchaseCancel()
+    {
+        closePanel();
+    }
+
+    private void openPanel()
+    {
+        purchasePanel.SetActive(true);
+        right.SetActive(false);
+        left.SetActive(false);
+        oriSet.SetActive(false);
+    }
+
+    private void closePanel()
+    {
+        purchasePanel.SetActive(false);
+        right.SetActive(true);
+        left.SetActive(true);
+        oriSet.SetActive(true);
+    }
+
+    public void checkState()
+    {
+        if (state[num] == 0)
+        {
+            oriSet.SetActive(true);
+            oriState.SetActive(true);
+            oriSet.GetComponent<Image>().sprite = setBG[0];
+            oriState.GetComponent<SpriteRenderer>().sprite = stateBG[0];
+        }
+
+        else if (state[num] == 1)
+        {
+            oriSet.SetActive(true);
+            oriState.SetActive(true);
+            oriSet.GetComponent<Image>().sprite = setBG[0];
+            oriState.GetComponent<SpriteRenderer>().sprite = stateBG[1];
+        }
+
+        else if (state[num] == 2)
+        {
+            oriSet.SetActive(true);
+            oriState.SetActive(true);
+            oriSet.GetComponent<Image>().sprite = setBG[1];
+            oriState.GetComponent<SpriteRenderer>().sprite = price[num];
+        }
+
+        else if (state[num] == 3)
+        {
+            oriSet.SetActive(false);
+            oriState.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(leftButton.isDown)
+        if (check)
         {
-            ScrollLeft();
+            checkState();
+            check = false;
         }
 
-        if (rightButton.isDown)
+        if (touchChk)
         {
-            ScrollRight();
+            right.SetActive(false);
+            left.SetActive(false);
+            oriSet.SetActive(false);
+            oriState.SetActive(false);
+            carrot.gameObject.SetActive(false);
+            gameObject.GetComponent<Transform>().localScale = new Vector3(1.5f, 1.5f);
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(clickPos[num], yPos);
+            //BackGround.GetComponent<Transform>().localScale = new Vector3(1.0f, 1.0f);
         }
-    }
 
-    private void ScrollLeft()
-    {
-        if(_scrollRect.horizontalNormalizedPosition >= 0f)
+        if (!touchChk)
         {
-            _scrollRect.horizontalNormalizedPosition -= scrollSpeed;
-        }
-    }
-
-    private void ScrollRight()
-    {
-        if (_scrollRect.horizontalNormalizedPosition <= 1f)
-        {
-            _scrollRect.horizontalNormalizedPosition += scrollSpeed;
+            right.SetActive(true);
+            left.SetActive(true);
+            carrot.gameObject.SetActive(true);
+            check = true;
+            gameObject.GetComponent<Transform>().localScale = new Vector3(1.0f, 1.0f);
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(xPos[num], yPos);
+            //BackGround.GetComponent<Transform>().localScale = new Vector3(0.6f, 0.6f);
         }
     }
 }
--- MoonBunny/Assets/Scripts/shop/clickBg.cs
+++ MoonBunny/Assets/Scripts/shop/clickBg.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class clickBg : MonoBehaviour
 {
     public GameObject purchasePanel;
     private bool panel;
+    private GraphicRaycaster gr;
+    private List<RaycastResult> results;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gr = GetComponent<GraphicRaycaster>();
     }
 
     // Update is called once per frame
@@ -20,14 +24,30 @@ public class clickBg : MonoBehaviour
             Vector3 p = Input.mousePosition;
             Ray cast = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
+        
             if (Physics.Raycast(cast, out hit))
             {
-                if (!panel && hit.collider.gameObject.tag == "BG")
+                if (hit.collider.gameObject.tag == "content")
                 {
-                    changeBg.touchChk = !changeBg.touchChk;
+                    ScrollView.touchChk = !ScrollView.touchChk;
+                    //hit.collider.gameObject.GetComponent<Transform>().localScale = new Vector3(1.5f, 1.5f);
                 }
             }
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            var ped = new PointerEventData(null);
+            ped.position = Input.mousePosition;
+            results = new List<RaycastResult>();
+            gr.Raycast(ped, results);
+        
+            if (results.Count <= 0) return;
+            if (!panel && results[0].gameObject.tag == "content" && results[0].gameObject.GetComponent<Image>().sprite.name != "ÀÚ¹°¼è")
+            {
+                ScrollView.touchChk = !ScrollView.touchChk;
+            }
+            //results[0].gameObject.transform.position = ped.position;
         }
 
         if (purchasePanel.activeSelf)
