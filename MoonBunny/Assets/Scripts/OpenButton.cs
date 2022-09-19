9460e29a56f9c1395aa33deb5205e58e8d823c55
Subin <ksb3458@naver.com>
2022-09-19 17:26:00 +0900

    배경음악, 효과음 삽입

--- MoonBunny/Assets/Scripts/OpenButton.cs
+++ MoonBunny/Assets/Scripts/OpenButton.cs
@@ -6,6 +6,7 @@ public class OpenButton : MonoBehaviour
 {
     public GameObject gm, timer, circle, level, life, open, score, lvTimer;
     public GameObject loadingUp, loadingDown;
+    public GameObject BGM1, BGM2;
     private bool openChk = false;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,8 @@ public class OpenButton : MonoBehaviour
         level.SetActive(false);
         life.SetActive(false);
         open.SetActive(false);
+        BGM1.SetActive(false);
+        BGM2.SetActive(false);
         //score.SetActive(false);
 
         gm.GetComponent<GameManager>().backGround.GetComponent<SpriteRenderer>().sprite = gm.GetComponent<GameManager>().BgSprites[PlayerPrefs.GetInt("ing_key")];
@@ -25,11 +28,12 @@ public class OpenButton : MonoBehaviour
 
     public void quitLoading()
     {
+        BGM1.SetActive(true);
+        BGM1.GetComponent<AudioSource>().Play();
         loadingUp.SetActive(false);
         open.SetActive(true);
         //score.SetActive(true);
     }
-
 
     public void Update()
     {
@@ -82,9 +86,12 @@ public class OpenButton : MonoBehaviour
         life.SetActive(true);
         open.SetActive(false);
         openChk = true;
+        BGM1.GetComponent<AudioSource>().Stop();
+        BGM1.SetActive(false);
+        BGM2.SetActive(true);
+        BGM2.GetComponent<AudioSource>().Play();
         //score.SetActive(true);
     }
 
     // Update is called once per frame
-
 }
