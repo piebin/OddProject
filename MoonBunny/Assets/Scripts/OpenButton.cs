f68dba24e78f91846c87d07140d3b63c807b47e7
piebin <76805688+piebin@users.noreply.github.com>
2022-09-20 19:44:41 +0900

    2번 오류 수정

--- MoonBunny/Assets/Scripts/OpenButton.cs
+++ MoonBunny/Assets/Scripts/OpenButton.cs
@@ -49,7 +49,7 @@ public class OpenButton : MonoBehaviour
                     gm.GetComponent<GameManager>().titlePanel.SetActive(true);
                     gm.GetComponent<GameManager>().dark.SetActive(true);
                     gm.GetComponent<GameManager>().goBack.GetComponent<AudioSource>().Play();
-                    gm.GetComponent<GameManager>().GuestBar.SetActive(false);
+                    //gm.GetComponent<GameManager>().GuestBar.SetActive(false);
                     lvTimer.GetComponent<Animator>().speed = 0.0f;
                     lvTimer.GetComponent<Animator>().enabled = false;
                     Time.timeScale = 0.0f;
