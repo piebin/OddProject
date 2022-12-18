36313a039cdf654eb2ce11335adf9f8eb5f0c3ea
piebin <76805688+piebin@users.noreply.github.com>
2022-12-19 03:28:45 +0900

    업적 확인용 테스트창
    업적 활성화 버튼(테스트용)

---
+++ MoonBunny/Assets/Scripts/test/testFileAchieve.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testFileAchieve : MonoBehaviour
+{
+   public void b1()
+    {
+        PlayerPrefs.SetInt("achieve_key0", 1);
+    }
+    public void b2()
+    {
+        PlayerPrefs.SetInt("achieve_key1", 1);
+    }
+    public void b3()
+    {
+        PlayerPrefs.SetInt("achieve_key2", 1);
+    }
+    public void b4()
+    {
+        PlayerPrefs.SetInt("achieve_key3", 1);
+    }
+    public void b5()
+    {
+        PlayerPrefs.SetInt("achieve_key4", 1);
+    }
+    public void b6()
+    {
+        PlayerPrefs.SetInt("achieve_key5", 1);
+    }
+    public void b7()
+    {
+        PlayerPrefs.SetInt("achieve_key6", 1);
+    }
+    public void b8()
+    {
+        PlayerPrefs.SetInt("achieve_key7", 1);
+    }
+    public void b9()
+    {
+        PlayerPrefs.SetInt("achieve_key8", 1);
+    }
+    public void b10()
+    {
+        PlayerPrefs.SetInt("achieve_key9", 1);
+    }
+
+    public void bA()
+    {
+        PlayerPrefs.SetInt("achieve_key0", 1);
+        PlayerPrefs.SetInt("achieve_key1", 1);
+        PlayerPrefs.SetInt("achieve_key2", 1);
+        PlayerPrefs.SetInt("achieve_key3", 1);
+        PlayerPrefs.SetInt("achieve_key4", 1);
+        PlayerPrefs.SetInt("achieve_key5", 1);
+        PlayerPrefs.SetInt("achieve_key6", 1);
+        PlayerPrefs.SetInt("achieve_key7", 1);
+        PlayerPrefs.SetInt("achieve_key8", 1);
+        PlayerPrefs.SetInt("achieve_key9", 1);
+    }
+
+}
--- MoonBunny/Assets/Scripts/titleClick.cs
+++ MoonBunny/Assets/Scripts/titleClick.cs
@@ -229,6 +229,11 @@ public class titleClick : MonoBehaviour
         PlayerPrefs.DeleteAll();
     }
 
+    public void goTest()
+    {
+        Debug.Log("테스트 씬으로 이동");
+        SceneManager.LoadScene("TestSceen(achieve)");
+    }
     public void GameQuit()
     {
 #if UNITY_EDITOR
