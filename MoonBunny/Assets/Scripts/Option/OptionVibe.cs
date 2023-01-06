bf1f928cb455a508d5e090e40aff79b4cf673964
Subin <ksb3458@naver.com>
2023-01-06 17:09:25 +0900

    현재 상태에 따라 진동버튼 색상 변경

--- MoonBunny/Assets/Scripts/Option/OptionVibe.cs
+++ MoonBunny/Assets/Scripts/Option/OptionVibe.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionVibe : MonoBehaviour
 {
+    private Color grey = new Color(147/255f, 147/255f, 147/255f);
+    public GameObject ON;
+    public GameObject OFF;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.GetInt("vibe") == 1)
+        {
+            ON.GetComponent<Image>().color = grey;
+            OFF.GetComponent<Image>().color = Color.white;
+        }
+
+        else
+        {
+            ON.GetComponent<Image>().color = Color.white;
+            OFF.GetComponent<Image>().color = grey;
+        }
     }
 
     // Update is called once per frame
     public void VibeOff()
     {
         PlayerPrefs.SetInt("vibe", 0);
+        ON.GetComponent<Image>().color = Color.white;
+        OFF.GetComponent<Image>().color = grey;
     }
 
     public void VibeOn()
     {
         PlayerPrefs.SetInt("vibe", 1);
+        ON.GetComponent<Image>().color = grey;
+        OFF.GetComponent<Image>().color = Color.white;
         Vibration.Vibrate((long) 120);
     }
 
